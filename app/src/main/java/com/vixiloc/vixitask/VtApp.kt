35ea0c93030413873313c4f21782c519c6dbf19e
Rizky Agung Prayogi <rizkyden06@gmail.com>
2023-08-23 13:40:44 +0700

    modify or add App class

---
+++ app/src/main/java/com/vixiloc/vixitask/VtApp.kt
@@ -0,0 +1,19 @@
+package com.vixiloc.vixitask
+
+import android.app.Application
+import com.vixiloc.vixitask.di.dbModule
+import com.vixiloc.vixitask.di.repositoryModule
+import com.vixiloc.vixitask.di.viewModelModule
+import org.koin.android.ext.koin.androidContext
+import org.koin.core.context.startKoin
+
+class VtApp : Application() {
+    override fun onCreate() {
+        super.onCreate()
+
+        startKoin{
+            androidContext(this@VtApp)
+            modules(listOf(viewModelModule, repositoryModule, dbModule))
+        }
+    }
+}
