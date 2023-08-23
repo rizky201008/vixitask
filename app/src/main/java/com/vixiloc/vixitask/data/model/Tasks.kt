282bb004bb95a79d395489073ebd55085efcc58e
Rizky Agung Prayogi <rizkyden06@gmail.com>
2023-08-23 13:40:29 +0700

    modify or add data class

---
+++ app/src/main/java/com/vixiloc/vixitask/data/model/Tasks.kt
@@ -0,0 +1,25 @@
+package com.vixiloc.vixitask.data.model
+
+import android.os.Parcelable
+import androidx.room.ColumnInfo
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+import kotlinx.parcelize.Parcelize
+
+@Entity(tableName = "tasks")
+@Parcelize
+data class Tasks(
+
+    @PrimaryKey(autoGenerate = true)
+    @ColumnInfo(name = "id")
+    val id: Int? = null,
+
+    @ColumnInfo(name = "title")
+    val title: String,
+
+    @ColumnInfo(name = "date")
+    val date: String,
+
+    @ColumnInfo(name = "isDone")
+    val isDone: Boolean = false
+) : Parcelable
