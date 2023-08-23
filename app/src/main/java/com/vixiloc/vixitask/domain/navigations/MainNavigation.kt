9ac7e04a9520a16fc3a36434145775c6a245f5bd
Rizky Agung Prayogi <rizkyden06@gmail.com>
2023-08-23 13:37:55 +0700

    modify navigations

--- app/src/main/java/com/vixiloc/vixitask/domain/navigations/MainNavigation.kt
+++ app/src/main/java/com/vixiloc/vixitask/domain/navigations/MainNavigation.kt
@@ -1,32 +1,125 @@
 package com.vixiloc.vixitask.domain.navigations
 
+import android.os.Build
+import androidx.annotation.RequiresApi
+import androidx.compose.animation.AnimatedContentTransitionScope
+import androidx.compose.animation.core.tween
 import androidx.compose.runtime.Composable
 import androidx.navigation.NavHostController
+import androidx.navigation.NavType
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
+import androidx.navigation.navArgument
+import com.vixiloc.vixitask.presentations.screens.AboutScreen
 import com.vixiloc.vixitask.presentations.screens.AddScreen
 import com.vixiloc.vixitask.presentations.screens.DetailScreen
 import com.vixiloc.vixitask.presentations.screens.HomeScreen
 import com.vixiloc.vixitask.presentations.screens.SplashScreen
 import com.vixiloc.vixitask.presentations.screens.UpdateScreen
 
+@RequiresApi(Build.VERSION_CODES.O)
 @Composable
-fun MainNavigation(navHostController: NavHostController) {
+fun MainNavigation(navHostController: NavHostController, onInterstitialRequest: () -> Unit) {
     NavHost(navController = navHostController, startDestination = MainDestination.Splash.route) {
-        composable(route = MainDestination.Splash.route) {
-            SplashScreen(navHostController = navHostController)
+        composable(
+            route = MainDestination.Splash.route,
+            exitTransition = {
+                slideOutOfContainer(
+                    towards = AnimatedContentTransitionScope.SlideDirection.Companion.Up,
+                    animationSpec = tween(700)
+                )
+            }
+        ) {
+            SplashScreen(
+                navHostController = navHostController,
+                onInterstitialRequest = { onInterstitialRequest() }
+            )
         }
-        composable(route = MainDestination.Home.route) {
+        composable(
+            route = MainDestination.Home.route,
+            exitTransition = {
+                slideOutOfContainer(
+                    towards = AnimatedContentTransitionScope.SlideDirection.Companion.Left,
+                    animationSpec = tween(700)
+                )
+            },
+        ) {
             HomeScreen(navHostController = navHostController)
         }
-        composable(route = MainDestination.Detail.route) {
-            DetailScreen(navHostController = navHostController)
+        composable(
+            route = MainDestination.Detail.route,
+            enterTransition = {
+                slideIntoContainer(
+                    towards = AnimatedContentTransitionScope.SlideDirection.Companion.Left,
+                    animationSpec = tween(700)
+                )
+            },
+            exitTransition = {
+                slideOutOfContainer(
+                    towards = AnimatedContentTransitionScope.SlideDirection.Companion.Left,
+                    animationSpec = tween(700)
+                )
+            },
+            arguments = listOf(navArgument("taskId") { type = NavType.IntType })
+        ) { backStackEntry ->
+            val id = backStackEntry.arguments?.getInt("taskId")
+            DetailScreen(
+                navHostController = navHostController,
+                taskId = id
+            )
         }
-        composable(route = MainDestination.Add.route) {
+        composable(
+            route = MainDestination.Add.route,
+            enterTransition = {
+                slideIntoContainer(
+                    towards = AnimatedContentTransitionScope.SlideDirection.Companion.Left,
+                    animationSpec = tween(700)
+                )
+            },
+            exitTransition = {
+                slideOutOfContainer(
+                    towards = AnimatedContentTransitionScope.SlideDirection.Companion.Left,
+                    animationSpec = tween(700)
+                )
+            },
+        ) {
             AddScreen(navHostController = navHostController)
         }
-        composable(route = MainDestination.Update.route) {
-            UpdateScreen(navHostController = navHostController)
+        composable(
+            route = MainDestination.Update.route,
+            enterTransition = {
+                slideIntoContainer(
+                    towards = AnimatedContentTransitionScope.SlideDirection.Companion.Left,
+                    animationSpec = tween(700)
+                )
+            },
+            exitTransition = {
+                slideOutOfContainer(
+                    towards = AnimatedContentTransitionScope.SlideDirection.Companion.Left,
+                    animationSpec = tween(700)
+                )
+            },
+            arguments = listOf(navArgument("taskId") { type = NavType.IntType })
+        ) { backStackEntry ->
+            val id = backStackEntry.arguments?.getInt("taskId")
+            UpdateScreen(navHostController = navHostController, taskId = id)
+        }
+        composable(
+            route = MainDestination.About.route,
+            enterTransition = {
+                slideIntoContainer(
+                    towards = AnimatedContentTransitionScope.SlideDirection.Companion.Left,
+                    animationSpec = tween(700)
+                )
+            },
+            exitTransition = {
+                slideOutOfContainer(
+                    towards = AnimatedContentTransitionScope.SlideDirection.Companion.Left,
+                    animationSpec = tween(700)
+                )
+            },
+        ) {
+            AboutScreen(navHostController = navHostController)
         }
     }
 }
@@ -34,7 +127,13 @@ fun MainNavigation(navHostController: NavHostController) {
 sealed class MainDestination(val route: String) {
     object Splash : MainDestination(route = "splash")
     object Home : MainDestination(route = "home")
-    object Detail : MainDestination(route = "detail")
+    object About : MainDestination(route = "about")
+    object Detail : MainDestination(route = "detail/{taskId}") {
+        fun createRoute(taskId: Int) = "detail/$taskId"
+    }
+
     object Add : MainDestination(route = "add")
-    object Update : MainDestination(route = "update")
+    object Update : MainDestination(route = "update/{taskId}") {
+        fun createRoute(taskId: Int) = "update/$taskId"
+    }
 }
