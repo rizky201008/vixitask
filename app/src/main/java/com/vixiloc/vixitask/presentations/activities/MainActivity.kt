21cbad017042064767998f8928480e3d874fecd4
Rizky Agung Prayogi <rizkyden06@gmail.com>
2023-09-03 19:34:43 +0700

    modify activity

--- app/src/main/java/com/vixiloc/vixitask/presentations/activities/MainActivity.kt
+++ app/src/main/java/com/vixiloc/vixitask/presentations/activities/MainActivity.kt
@@ -2,11 +2,9 @@ package com.vixiloc.vixitask.presentations.activities
 
 import android.os.Build
 import android.os.Bundle
-import android.util.Log
 import android.view.WindowInsets
 import android.view.WindowInsetsController
 import android.view.WindowManager
-import android.widget.Toast
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.annotation.RequiresApi
@@ -17,16 +15,8 @@ import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Modifier
 import androidx.core.view.WindowCompat
 import androidx.navigation.compose.rememberNavController
-import com.google.android.gms.ads.AdError
-import com.google.android.gms.ads.AdRequest
-import com.google.android.gms.ads.FullScreenContentCallback
-import com.google.android.gms.ads.LoadAdError
-import com.google.android.gms.ads.interstitial.InterstitialAd
-import com.google.android.gms.ads.interstitial.InterstitialAdLoadCallback
-import com.vixiloc.vixitask.BuildConfig
 import com.vixiloc.vixitask.domain.navigations.MainNavigation
 import com.vixiloc.vixitask.ui.theme.VixitaskTheme
-import kotlinx.coroutines.launch
 
 class MainActivity : ComponentActivity() {
     @RequiresApi(Build.VERSION_CODES.O)
@@ -58,49 +48,9 @@ class MainActivity : ComponentActivity() {
                     val navHostController = rememberNavController()
                     MainNavigation(
                         navHostController = navHostController,
-                        onInterstitialRequest = {
-                            scope.launch {
-                                showInterstialAd()
-                            }
-                        }
                     )
                 }
             }
         }
-    }
-
-    private fun showInterstialAd() {
-       /* val adunit = BuildConfig.INTERSTITIAL_KEY
-        InterstitialAd.load(
-            this,
-            adunit,
-            AdRequest.Builder().build(),
-            object : InterstitialAdLoadCallback() {
-                override fun onAdFailedToLoad(adError: LoadAdError) {
-                }
-
-                override fun onAdLoaded(interstitialAd: InterstitialAd) {
-                    interstitialAd.fullScreenContentCallback =
-                        object : FullScreenContentCallback() {
-                            override fun onAdDismissedFullScreenContent() {
-                                super.onAdDismissedFullScreenContent()
-                                // Show a toast here indicating that the ad has been dismissed
-                            }
-
-                            override fun onAdFailedToShowFullScreenContent(adError: AdError) {
-                                super.onAdFailedToShowFullScreenContent(adError)
-                            }
-
-                            override fun onAdShowedFullScreenContent() {
-                                super.onAdShowedFullScreenContent()
-                                // Ad has been shown, you can log this if needed
-                            }
-
-                        }
-                    interstitialAd.show(this@MainActivity)
-                }
-            }
-        )
-        */
     }
 }
