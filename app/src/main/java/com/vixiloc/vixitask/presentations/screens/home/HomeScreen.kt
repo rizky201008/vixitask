3e4d19336a28b0e81d9f2a5a7213ae2de9990839
Rizky Agung Prayogi <rizkyden06@gmail.com>
2023-10-30 14:04:24 +0700

    modify screens

--- app/src/main/java/com/vixiloc/vixitask/presentations/screens/home/HomeScreen.kt
+++ app/src/main/java/com/vixiloc/vixitask/presentations/screens/home/HomeScreen.kt
@@ -3,27 +3,17 @@ package com.vixiloc.vixitask.presentations.screens.home
 import android.content.Intent
 import android.os.Build
 import androidx.annotation.RequiresApi
-import androidx.compose.animation.AnimatedVisibility
 import androidx.compose.foundation.ExperimentalFoundationApi
 import androidx.compose.foundation.background
-import androidx.compose.foundation.border
-import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
-import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.outlined.AddCircle
-import androidx.compose.material.icons.outlined.Info
-import androidx.compose.material.icons.outlined.Search
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.ExperimentalMaterial3Api
-import androidx.compose.material3.Icon
-import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
@@ -33,12 +23,13 @@ import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.rememberCoroutineScope
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.zIndex
 import androidx.navigation.NavHostController
 import androidx.navigation.compose.rememberNavController
 import com.vixiloc.vixitask.R
@@ -46,8 +37,6 @@ import com.vixiloc.vixitask.presentations.MainDestination
 import com.vixiloc.vixitask.presentations.components.NothingToShow
 import com.vixiloc.vixitask.presentations.components.TaskActiveCard
 import com.vixiloc.vixitask.presentations.components.TaskInactiveCard
-import com.vixiloc.vixitask.presentations.components.TextFieldSearch
-import com.vixiloc.vixitask.presentations.components.TopBarTransparent
 import com.vixiloc.vixitask.presentations.ui.theme.VixitaskTheme
 import kotlinx.coroutines.launch
 import org.koin.androidx.compose.getViewModel
@@ -60,167 +49,13 @@ fun HomeScreen(navHostController: NavHostController, viewModel: HomeScreenVm = g
     val list by viewModel.tasks.collectAsState(initial = emptyList())
     val context = LocalContext.current
 
-    Scaffold(topBar = {
-        Column(modifier = Modifier.fillMaxWidth()) {
-            TopBarTransparent(
-                icon = {
-                    IconButton(onClick = { navHostController.navigate(MainDestination.About.route) }) {
-                        Icon(
-                            imageVector = Icons.Outlined.Info,
-                            contentDescription = "Information",
-                            modifier = Modifier.size(30.dp)
-                        )
-                    }
-                },
-                actions = {
-                    IconButton(onClick = { visible.value = !visible.value }) {
-                        Icon(
-                            imageVector = Icons.Outlined.Search,
-                            contentDescription = "Search",
-                            modifier = Modifier.size(30.dp)
-                        )
-                    }
-                    IconButton(onClick = { navHostController.navigate(MainDestination.Add.route) }) {
-                        Icon(
-                            imageVector = Icons.Outlined.AddCircle,
-                            contentDescription = "Create",
-                            modifier = Modifier
-                                .padding(start = 10.dp)
-                                .size(35.dp)
-                        )
-                    }
-                }
-            )
-            val searchValue = viewModel.searchValue.collectAsState().value
-            AnimatedVisibility(visible = visible.value) {
-                TextFieldSearch(
-                    modifier = Modifier.padding(horizontal = 30.dp),
-                    value = searchValue,
-                    onChange = {
-                        viewModel.search(it)
-                    },
-                )
-            }
-        }
-    }) { paddingValues ->
-        LazyColumn(modifier = Modifier.padding(paddingValues)) {
-            stickyHeader {
-                Box(
-                    modifier = Modifier
-                        .fillMaxWidth()
-                        .background(color = MaterialTheme.colorScheme.background)
-                        .padding(10.dp)
-                ) {
-                    Text(
-                        text = stringResource(R.string.active_tasks),
-                        style = MaterialTheme.typography.bodySmall
-                    )
-                }
-            }
+    Box(
+        modifier = Modifier
+    ) {
 
-            val activeTasks = list.filter { !it.isDone }
-            val inActiveTasks = list.filter { it.isDone }
-
-            if (activeTasks.isEmpty()) {
-                item {
-                    NothingToShow()
-                }
-            } else {
-                items(activeTasks) { task ->
-                    TaskActiveCard(
-                        title = task.title,
-                        dateTime = task.date,
-                        onClick = {
-                            scope.launch {
-                                navHostController.navigate(MainDestination.Detail.createRoute(task.id!!))
-                            }
-                        },
-                        modifier = Modifier.animateItemPlacement(),
-                        onDone = {
-                            scope.launch {
-                                viewModel.done(task.id!!)
-                            }
-                        },
-                        onDelete = {
-                            scope.launch {
-                                viewModel.delete(task)
-                            }
-                        },
-                        onShare = {
-                            val sendIntent: Intent = Intent().apply {
-                                action = Intent.ACTION_SEND
-                                putExtra(
-                                    Intent.EXTRA_TEXT,
-                                    "Hello, this is my task:\n" +
-                                            "Title : ${task.title}\n" +
-                                            "At : ${task.date}"
-                                )
-                                type = "text/plain"
-                            }
-
-                            val shareIntent = Intent.createChooser(sendIntent, null)
-                            context.startActivity(shareIntent)
-                        }
-                    )
-                }
-            }
-
-            stickyHeader {
-                Box(
-                    modifier = Modifier
-                        .fillMaxWidth()
-                        .background(color = MaterialTheme.colorScheme.background)
-                        .padding(10.dp)
-                ) {
-                    Text(
-                        text = stringResource(R.string.inactive_tasks),
-                        style = MaterialTheme.typography.bodySmall
-                    )
-                }
-            }
-
-            if (inActiveTasks.isEmpty()) {
-                item {
-                    NothingToShow()
-                }
-            } else {
-                items(inActiveTasks) { task ->
-                    TaskInactiveCard(
-                        title = task.title,
-                        dateTime = task.date,
-                        onClick = {
-                            scope.launch {
-                                navHostController.navigate(MainDestination.Detail.createRoute(task.id!!))
-                            }
-                        },
-                        modifier = Modifier.animateItemPlacement(),
-                        onDelete = {
-                            scope.launch {
-                                viewModel.delete(task)
-                            }
-                        },
-                        onShare = {
-                            val sendIntent: Intent = Intent().apply {
-                                action = Intent.ACTION_SEND
-                                putExtra(
-                                    Intent.EXTRA_TEXT,
-                                    "Task : ${task.title}\n" +
-                                            "At : ${task.date}"
-                                )
-                                type = "text/plain"
-                            }
-
-                            val shareIntent = Intent.createChooser(sendIntent, null)
-                            context.startActivity(shareIntent)
-                        }
-                    )
-                }
-            }
-        }
     }
 }
 
-@RequiresApi(Build.VERSION_CODES.O)
 @Preview
 @Composable
 fun HomeScreenPreview() {
